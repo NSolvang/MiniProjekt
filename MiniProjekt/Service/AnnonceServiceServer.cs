1ae96b8006ef881a003f59c720da2a9bce1a7d60
NSolvang <niko6041@hotmail.com>
2025-04-24 09:56:23 +0200

    ServiceServer

--- MiniProjekt/Service/AnnonceServiceServer.cs
+++ MiniProjekt/Service/AnnonceServiceServer.cs
@@ -1,6 +1,31 @@
+using System.Net.Http.Json;
+using Core;
+
 namespace MiniProjekt.Service;
 
-public class AnnonceServiceServer
+public class AnnonceServiceServer : IAnnonceService
 {
+    private string serverUrl = "http://localhost:5238/";
     
+    private HttpClient client;
+
+    private AnnonceServiceServer(HttpClient client)
+    {
+        this.client = client;
+    }
+    
+    public async Task<Annonce[]> GetAll()
+    {
+        return await client.GetFromJsonAsync<Annonce[]>($"{serverUrl}/api/bike");
+    }
+
+    public async Task Add(Annonce annonce)
+    {
+        await client.PostAsJsonAsync<Annonce>($"{serverUrl}/api/bike", annonce);
+    }
+
+    public async Task DeleteById(int id)
+    {
+        await client.DeleteAsync($"{serverUrl}/api/bike/{id}");
+    }
 }
