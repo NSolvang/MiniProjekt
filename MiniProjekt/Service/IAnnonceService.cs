5498eb9711c50d4bba23a94af1c8d5d4cfb2884d
NSolvang <niko6041@hotmail.com>
2025-04-24 09:57:02 +0200

    IannonceService

--- MiniProjekt/Service/IAnnonceService.cs
+++ MiniProjekt/Service/IAnnonceService.cs
@@ -1,6 +1,9 @@
+using Core;
 namespace MiniProjekt.Service;
 
 public interface IAnnonceService
 {
-    
+    Task<Annonce[]> GetAll();
+    Task Add(Annonce annonce);
+    Task DeleteById(int id);
 }
